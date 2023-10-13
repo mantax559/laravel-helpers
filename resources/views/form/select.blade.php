f37c50ef5fbccce421628e8a2d089feb78667dcb
Mantasblo <mantax559@gmail.com>
2023-10-13 16:49:44 +0300

    Select component added

---
+++ resources/views/form/select.blade.php
@@ -0,0 +1,161 @@
+<x-form::input-group :name="$name" :class="$class" :append="$append">
+    @isset($title)
+        <x-form::label :id="$id" :title="$title" :required="$required"/>
+    @endisset
+
+    @isset($tooltip)
+        <x-form::tooltip :title="$tooltip"/>
+    @endisset
+
+    <select {{ $attributes->merge($inputAttributes)->class([config('laravel-helpers.form.select.class'), config('laravel-helpers.form.error.input_class') => $hasError($name)]) }} />
+</x-form::input-group>
+
+{{-- TODO: Hardcode --}}
+<script type="text/javascript">
+    settings = {
+        multiple: {{ $multiple ? 'true' : 'false' }},
+        theme: 'bootstrap-5',
+        language: '{{ $locale }}',
+        width: '100%',
+        placeholder: '{{ $placeholder }}',
+        allowClear: {{ $multiple && !$required ? 'true' : 'false' }},
+    }
+
+    @if(!empty($data))
+        @if(is_more_or_equal(config('laravel-helpers.form.select.minimum_results_for_search'), count($data)))
+            settings.minimumResultsForSearch = Infinity;
+        @endif
+        settings.data = @json($data);
+    @elseif(!empty($api))
+        settings.ajax = {
+            url: '{{ $api }}',
+            type: 'post',
+            dataType: 'json',
+            delay: 100,
+            data: function (params) {
+                return {
+                    query: params.term,
+                    page: params.page || 1,
+                    _token: '{{ csrf_token() }}',
+                };
+            },
+            cache: true
+        };
+    @endif
+
+    $('#{{ $id }}').select2(settings).val(null).trigger('change');
+
+    @if(!empty($value) || cmprstr($value, 0) || isset($disabled))
+        $('#{{ $id }}').prop('disabled', true);
+        @if(!empty($value) || cmprstr($value, 0))
+            values = @json($value);
+            @if(isset($data))
+                $('#{{ $id }}').val(values).trigger('change').prop('disabled', false);
+            @elseif(isset($api))
+                data = new FormData();
+                data.append('values', values);
+                data.append('_token', '{{ csrf_token() }}');
+
+                $.ajax({
+                    url: '{{ $api }}',
+                    type: 'post',
+                    dataType: 'json',
+                    data: data,
+                    cache: false,
+                    processData: false,
+                    contentType: false,
+                }).then(function (data) {
+                    data.forEach(function (item) {
+                        $('#{{ $id }}').append(new Option(item.text, item.id, true, true));
+                    });
+                    $('#{{ $id }}').prop('disabled', false);
+                });
+            @endif
+        @endif
+    @endif
+</script>
+
+@push('scripts')
+    <script type="text/javascript">
+        settings = {
+            multiple: {{ $multiple ? 'true' : 'false' }},
+            theme: 'bootstrap-5',
+            language: '{{ $locale }}',
+            width: '100%',
+            placeholder: '{{ $placeholder }}',
+            allowClear: {{ $multiple && !$required ? 'true' : 'false' }},
+        }
+
+        @if(!empty($data))
+            @if(is_more_or_equal(config('laravel-helpers.form.select.minimum_results_for_search'), count($data)))
+                settings.minimumResultsForSearch = Infinity;
+            @endif
+            settings.data = @json($data);
+        @elseif(!empty($api))
+            settings.ajax = {
+                url: '{{ $api }}',
+                type: 'post',
+                dataType: 'json',
+                delay: 100,
+                data: function (params) {
+                    return {
+                        query: params.term,
+                        page: params.page || 1,
+                        _token: '{{ csrf_token() }}',
+                    };
+                },
+                cache: true
+            };
+        @endif
+
+        $('#{{ $id }}').select2(settings).val(null).trigger('change');
+
+        @if(!empty($value) || cmprstr($value, 0) || isset($disabled))
+            $('#{{ $id }}').prop('disabled', true);
+            @if(!empty($value) || cmprstr($value, 0))
+                values = @json($value);
+                @if(isset($data))
+                    $('#{{ $id }}').val(values).trigger('change').prop('disabled', false);
+                @elseif(isset($api))
+                    data = new FormData();
+                    data.append('values', values);
+                    data.append('_token', '{{ csrf_token() }}');
+
+                    $.ajax({
+                        url: '{{ $api }}',
+                        type: 'post',
+                        dataType: 'json',
+                        data: data,
+                        cache: false,
+                        processData: false,
+                        contentType: false,
+                    }).then(function (data) {
+                        data.forEach(function (item) {
+                            $('#{{ $id }}').append(new Option(item.text, item.id, true, true));
+                        });
+                        $('#{{ $id }}').prop('disabled', false);
+                    });
+                @endif
+            @endif
+        @endif
+
+        @if(isset($wireModel) || isset($wireModelDefer))
+            $(document).ready(function () {
+                $('#{{ $id }}').on('change', function (e) {
+                    @this.set('{{ $convertBracketsToDots($name) }}', $('#{{ $id }}').select2("val"));
+                });
+            });
+        @endif
+    </script>
+@endpush
+
+@once
+    @push('cdn-header')
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css"/>
+    @endpush
+    @push('cdn-footer')
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/i18n/{{ $locale }}.js"></script>
+    @endpush
+@endonce
---
+++ src/View/Components/Forms/Select.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Mantax559\LaravelHelpers\View\Components\Forms;
+
+class Select extends FormComponent
+{
+    public function __construct(
+        string $name,
+        string $class = null,
+        string $id = null,
+        string $selected = null,
+        string $title = null,
+        string $placeholder = null,
+        string $tooltip = null,
+        string $autocomplete = null,
+        string $append = null,
+        bool $autofocus = false,
+        bool $disabled = false,
+        bool $required = false,
+        public ?string $wireModel = null,
+        public ?string $wireModelDefer = null,
+        public ?string $api = null,
+        public array $data = [],
+        public bool $multiple = false,
+    ) {
+        $this->locale = $this->locale ?? app()->getLocale();
+
+        parent::__construct(
+            inputAttributes: [
+                'wire:model' => $this->wireModel,
+                'wire:model.defer' => $this->wireModelDefer,
+            ],
+            name: $name,
+            class: $class,
+            id: $id,
+            value: $selected,
+            title: $title,
+            placeholder: $placeholder,
+            tooltip: $tooltip,
+            autocomplete: $autocomplete,
+            append: $append,
+            autofocus: $autofocus,
+            disabled: $disabled,
+            required: $required
+        );
+    }
+}
