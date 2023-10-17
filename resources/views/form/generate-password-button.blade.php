76d56cb3e726dd6cd1b4eb1b963e7270ed1d12d6
Mantasblo <mantax559@gmail.com>
2023-10-17 22:08:19 +0300

    Bugfix

--- resources/views/form/generate-password-button.blade.php
+++ resources/views/form/generate-password-button.blade.php
@@ -23,24 +23,28 @@
                 uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
                 lowercase = 'abcdefghijklmnopqrstuvwxyz',
                 numbers = '0123456789',
-                symbol = '~`!@#$%^&*()_-+={[}]|\:;"\'<,>.?/',
-                charset = lowercase + uppercase + numbers + symbol,
+                symbols = '~`!@#$%^&*()_-+={[}]|\:;"\'<,>.?/',
+                charset = uppercase + lowercase + numbers + symbols,
                 password = '';
 
             for (var i = 0, n = charset.length; i < length; ++i) {
                 password += charset.charAt(getRandomInt(n));
             }
 
-            password[getRandomInt(length)] = uppercase.charAt(getRandomInt(uppercase.length));
-            password[getRandomInt(length)] = lowercase.charAt(getRandomInt(lowercase.length));
-            password[getRandomInt(length)] = numbers.charAt(getRandomInt(numbers.length));
-            password[getRandomInt(length)] = symbol.charAt(getRandomInt(symbol.length));
+            password = password.replaceAt(getRandomInt(length), uppercase.charAt(getRandomInt(uppercase.length)))
+            password = password.replaceAt(getRandomInt(length), lowercase.charAt(getRandomInt(lowercase.length)))
+            password = password.replaceAt(getRandomInt(length), numbers.charAt(getRandomInt(numbers.length)))
+            password = password.replaceAt(getRandomInt(length), symbols.charAt(getRandomInt(symbols.length)))
 
             return password;
         }
 
         function getRandomInt(max) {
             return Math.floor(Math.random() * max);
+        }
+
+        String.prototype.replaceAt = function(index, replacement) {
+            return this.substring(0, index) + replacement + this.substring(index + replacement.length);
         }
     </script>
 @endpush
