1d03bb454958315b074ae40df3c4f9092545aef9
Mantasblo <mantax559@gmail.com>
2024-01-07 18:36:41 +0200

    whereLike added

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace Mantax559\LaravelHelpers\Providers;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,29 @@ class AppServiceProvider extends ServiceProvider
         $this->loadViewsFrom(self::PATH_VIEWS, 'laravel-helpers');
 
         Blade::componentNamespace('Mantax559\\LaravelHelpers\\View\\Components\\Forms', 'form');
+
+        // TODO: Move to seperate class
+        Builder::macro('whereLike', function ($attributes, string $search) {
+            $this->where(function (Builder $query) use ($attributes, $search) {
+                foreach (Arr::wrap($attributes) as $attribute) {
+                    $query->when(
+                        str_contains($attribute, '.'),
+                        function (Builder $query) use ($attribute, $search) {
+                            [$relationName, $relationAttribute] = explode('.', $attribute);
+
+                            $query->orWhereHas($relationName, function (Builder $query) use ($relationAttribute, $search) {
+                                $query->where($relationAttribute, 'LIKE', "%{$search}%");
+                            });
+                        },
+                        function (Builder $query) use ($attribute, $search) {
+                            $query->orWhere($attribute, 'LIKE', "%{$search}%");
+                        }
+                    );
+                }
+            });
+
+            return $this;
+        });
     }
 
     public function register(): void
