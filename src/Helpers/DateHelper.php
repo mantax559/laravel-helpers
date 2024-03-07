6ca01b51fade14151fac975c1843913fcd98dd20
Mantasblo <mantax559@gmail.com>
2024-03-07 16:06:26 +0200

    Create DateHelper.php

---
+++ src/Helpers/DateHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Helpers;
+
+use Carbon\Carbon;
+
+class DateHelper
+{
+    public static function toUtcString(Carbon $datetime): string
+    {
+        return $datetime->copy()->setTimezone('UTC')->toDateTimeString();
+    }
+
+    public static function fromUtcString(Carbon $datetime, string $timezone = 'UTC'): string
+    {
+        return $datetime->copy()->setTimezone($timezone)->toDateTimeString();
+    }
+}
