682277e01235e4d53703eed29af683bbabbc4b4f
Mantasblo <mantax559@gmail.com>
2024-06-07 14:52:22 +0300

    Update Select2Request.php

--- src/Requests/Select2Request.php
+++ src/Requests/Select2Request.php
@@ -7,11 +7,6 @@ use Mantax559\LaravelHelpers\Helpers\ValidationHelper;
 
 class Select2Request extends FormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     public function rules(): array
     {
         return [
